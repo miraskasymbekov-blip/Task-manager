ffc177fe4b8b26b8ce83481fbf8b72c9077691d3
miraskasymbekov-blip <miras.kasymbekov@gmail.com>
2026-06-29 22:22:44 +0500

    Initial commit: Настройка базы данных, создание сущности Task и репозитория

---
+++ src/main/java/com/miras/taskmanager/entity/Task.java
@@ -0,0 +1,38 @@
+package com.miras.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDateTime createdAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        if (this.status == null) {
+            this.status = TaskStatus.NEW;
+        }
+    }
+}
---
+++ src/main/java/com/miras/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.miras.taskmanager.repository;
+
+import com.miras.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
